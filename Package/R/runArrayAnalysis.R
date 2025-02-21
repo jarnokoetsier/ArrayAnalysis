bf1669c694c649c191e4ba7589e0b6d3857b5965
Jarno Koetsier <jarno.koetsier@gmail.com>
2025-02-21 09:25:53 +0100

    Update R package

--- Package/R/runArrayAnalysis.R
+++ Package/R/runArrayAnalysis.R
@@ -2,11 +2,15 @@
 #' @return This function will run the ArrayAnalysis app
 #' @examples runArrayAnalysis()
 #' @export
-runArrayAnalysis <- function(){
+runArrayAnalysis <- function(force.browser = FALSE){
   appDir <- system.file("myapp", package = "ArrayAnalysis")
   if (appDir == "") {
-    stop("Could not find myapp. Try re-installing `mypackage`.", call. = FALSE)
+    stop("Could not find myapp. Try re-installing `ArrayAnalysis`.", call. = FALSE)
   }
   
-  shiny::runApp(appDir, display.mode = "normal")
+  if (force.browser == FALSE){
+    shiny::runApp(appDir, display.mode = "normal")
+  } else{
+    shiny::runApp(appDir, display.mode = "normal", launch.browser = TRUE)
+  }
 }
